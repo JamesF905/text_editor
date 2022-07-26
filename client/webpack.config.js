76132add0542e0c5381d921d7a8ea76d937d6fdd
James <jameslfidlin@gmail.com>
2022-07-26 17:05:44 -0400

    fixed uppercase name tag in package.json and webpack files

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'JATE',
+        title: 'jate',
       }),
       new InjectManifest({
         swSrc: "./src-sw.js",
@@ -27,7 +27,7 @@ module.exports = () => {
         fingerprints: false,
         inject: true,
         name: "Just Another Text Editor",
-        short_name: "JATE",
+        short_name: "jate",
         description: "Takes notes with JavaScript syntax highlighting!",
         start_url: "/",
         publicPath: "/",
